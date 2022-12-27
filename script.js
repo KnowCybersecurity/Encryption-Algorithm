7069f614b01855b08e9f540b6113fae30ae25874
KnowCybersecurity <121370426+KnowCybersecurity@users.noreply.github.com>
2022-12-27 03:24:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -65,8 +65,8 @@ function Encrypt(){
         }
     }
 
-    document.getElementById("Ciphertext_Output").innerText = `Encrypted message: ${Ciphertext_Output}`;
-    document.getElementById("SecretKey_Output").innerText = `Secret Key: ${SecretKey_Output}`;
+    document.getElementById("Ciphertext_Output").innerText = `Encrypted message:\n${Ciphertext_Output}`;
+    document.getElementById("SecretKey_Output").innerText = `Secret Key:\n${SecretKey_Output}`;
 
 }
 
@@ -98,12 +98,12 @@ function Decrypt(){
         Plaintext_Output += SET1[DecryptedLetterIndex];
     }
 
-    document.getElementById("Plaintext_Output").innerText = `Decrypted message: ${Plaintext_Output}`;
+    document.getElementById("Plaintext_Output").innerText = `Decrypted message:\n ${Plaintext_Output}`;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////
 
-const SET1 = [" ", "?", "@", "%", ".", ",", "!", "$", "-", "'", "_", "+", "=", "/", "*", '"', "<", ">", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const SET1 = [" ", "?", "(", ")", ":", ";", "#", "{", "}", "[", "]", "&", "@", "%", ".", ",", "!", "$", "-", "'", "_", "+", "=", "/", "*", '"', "<", ">", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let SET2 = [];
 
 let ShiftValues = [];
